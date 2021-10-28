5920899c639ad06e722c8c6564f4845fa1394777
viki247 <73943757+viki247@users.noreply.github.com>
2021-10-28 11:44:04 +0530

    Update LogMelSpectrogram.py

--- LogMelSpectrogram.py
+++ LogMelSpectrogram.py
@@ -4,7 +4,6 @@
 #   This software component is licensed by ST under BSD 3-Clause license,
 #   the "License"; You may not use this file except in compliance with the
 #   License. You may obtain a copy of the License at:
-#                        https://opensource.org/licenses/BSD-3-Clause
 
 
 """LogMel Feature Extraction example."""
